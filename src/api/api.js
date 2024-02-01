10a6f1ab9338554f2f75a3145d99704f3b9019e6
Stephen Moss <bladexr8@gmail.com>
2024-02-02 09:16:31 +1100

    add abort functionality

--- src/api/api.js
+++ src/api/api.js
@@ -10,14 +10,62 @@ const axiosParams = {
 // Create axios instance with default params
 const axiosInstance = axios.create(axiosParams)
 
+// returns a boolean if an error object passed is an instance of the
+// Cancel error thrown by Axios
+export const didAbort = (error) => axios.isCancel(error)
+
+// assert if an error is an Axios error
+export const isApiError = (error) => {
+  return axios.isAxiosError(error)
+}
+
+// checks if the config object contains an abort property with a function
+// as a value. If so, create a new abort controller, set its signal
+// on the config object, and pass the abort method
+const withAbort = (fn) => {
+  const executor = async (...args) => {
+    const originalConfig = args[args.length - 1]
+    // Extract abort property from the config
+    const { abort: setAbortFn, ...config } = originalConfig
+
+    // Create cancel token and abort method only if
+    // abort function was passed
+    if (typeof setAbortFn === 'function') {
+      const abortController = new AbortController()
+      config.signal = abortController.signal
+      setAbortFn(() => {
+        abortController.abort()
+      })
+    }
+
+    try {
+      if (args.length > 2) {
+        const [url, body] = args
+        return await fn(url, body, config)
+      } else {
+        const [url] = args
+        return await fn(url, config)
+      }
+    } catch (error) {
+      // add "aborted" property to the error
+      // if the request was cancelled
+      if (didAbort(error)) {
+        error.aborted = true
+      }
+      throw error
+    }
+  }
+  return executor
+}
+
 // Main api function
 const api = (axios) => {
   return {
-    get: (url, config = {}) => axios.get(url, config),
-    delete: (url, config = {}) => axios.get(url, config),
-    post: (url, body, config = {}) => axios.post(url, body, config),
-    patch: (url, body, config = {}) => axios.post(url, body, config),
-    put: (url, body, config = {}) => axios.post(url, body, config),
+    get: (url, config = {}) => withAbort(axios.get)(url, config),
+    delete: (url, config = {}) => withAbort(axios.get)(url, config),
+    post: (url, body, config = {}) => withAbort(axios.post)(url, body, config),
+    patch: (url, body, config = {}) => withAbort(axios.post)(url, body, config),
+    put: (url, body, config = {}) => withAbort(axios.post)(url, body, config),
   }
 }
 
